dbd0b616c0e6f25d2ed7b6de2be7479e8b28c83c
VyseMACHINE <40603631+VyseMACHINE@users.noreply.github.com>
2019-07-01 21:41:09 +0600

    Add files via upload

---
+++ hw3-master/HomeWork3/HomeWork3/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace HomeWork3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Buyer",
+            "Seller",
+            "Sale"});
+            this.comboBox1.Location = new System.Drawing.Point(298, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(219, 32);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Выберите таблицу";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(125, 119);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(561, 244);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(676, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Создать таблицы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ hw3-master/HomeWork3/HomeWork3/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+
+namespace HomeWork3
+{
+    public partial class Form1 : Form
+    {
+        public static string dp = "System.Data.SqlClient";
+        public string cnStr = @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=Sales;Integrated Security=True";
+        public DbProviderFactory df = DbProviderFactories.GetFactory(dp);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            using (DbConnection cn = df.CreateConnection())
+            {
+                Console.WriteLine("DbConnection");
+                cn.ConnectionString = cnStr;
+                cn.Open();
+
+                DbCommand cmd = df.CreateCommand();
+                Console.WriteLine("DbCommand");
+                cmd.Connection = cn;
+                cmd.CommandText = "Select * from "+comboBox1.Text;
+                listBox1.Items.Add("Содержимое таблицы " + comboBox1.Text);
+                if (comboBox1.SelectedIndex == 2)
+                {
+                    using (DbDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            listBox1.Items.Add("Id: "+ dr[0] + "  BuyerName: "+ dr[1] + "  SellerId: "+ dr[2] + "  Summa: "+ dr[3] + "  Date: "+dr[4]);
+                        }
+                    }
+                }
+                else
+                {
+                    using (DbDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            listBox1.Items.Add("Id: " + dr[0] + "  Name: " + dr[1] + "  SecondName: " + dr[2]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (DbConnection cn = df.CreateConnection())
+            {
+                cn.ConnectionString = cnStr;
+                cn.Open();
+                
+                try
+                {
+                    //buyer
+                    DbCommand cmd = df.CreateCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandText = "Create table Buyer(Id int primary key identity(1,1), Name nvarchar(50), SecondName nvarchar(50))";
+                    int test = cmd.ExecuteNonQuery();
+
+                    //seller
+                    DbCommand cmd2 = df.CreateCommand();
+                    cmd2.Connection = cn;
+                    cmd2.CommandText = "Create table Seller(Id int primary key identity(1,1), Name nvarchar(50), SecondName nvarchar(50))";
+                    int test2 = cmd2.ExecuteNonQuery();
+                
+
+                    //seller
+                    DbCommand cmd3 = df.CreateCommand();
+                    cmd3.Connection = cn;
+                    cmd3.CommandText = "Create table Sale(Id int primary key identity(1,1), BuyerId int foreign key references Buyer(Id), SellerId int foreign key references Seller(Id)," +
+                        "Summa int,Date DateTime)";
+                    int test3 = cmd3.ExecuteNonQuery();
+                    
+                }
+                catch (Exception) { MessageBox.Show("Таблицы уже созданы или созданы с ошибкой"); }
+                cn.Close();
+            }
+        }
+    }
+}
